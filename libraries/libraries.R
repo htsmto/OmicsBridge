52b345591c910089671345c7f6d1096085d28a0d
htsmto <hitoshi.matsuo@dkfz-heidelberg.de>
2026-06-17 18:12:53 +0200

    Refactor app structure

---
+++ libraries/libraries.R
@@ -0,0 +1,13 @@
+
+cat("Loading libraries...\n")
+
+suppressMessages(library(shiny))
+suppressMessages(library(shinyjs))
+suppressMessages(library(shinydashboard))
+suppressMessages(library(shinyWidgets))
+suppressMessages(library(shinycssloaders))
+suppressMessages(library(ggplot2))
+suppressMessages(library(ggrepel))
+suppressMessages(library(DT))
+
+cat("Libraries loaded.\n")
